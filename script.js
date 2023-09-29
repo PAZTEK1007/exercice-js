a4c536854be272e202e3498f2d1372fe5ea0b173
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-29 12:40:38 +0200

    push exercice files

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let keller = ["K", "E", "L", "L", "E", "R"];
-    i = 0;
-    id = setInterval(function() {
-        console.log(keller [i++]);
-        if (i === keller.length) clearInterval(id);
-    }, 1000);
+let firstTime = Date.now();
+
+function timer (){
+    let actualTime = Date.now();
+    let timeout = actualTime - firstTime;
+    let secondTimeout = Math.floor((timeout % 60000) / 1000);
+    let minuteTimeout = Math.floor(timeout / 60000);
+
+    let message;
+    if (secondTimeout === 1){
+        message = "a second has passed !";
+    } else if (minuteTimeout === 1){
+        message = "a minute " + "and" + secondTimeout + " has passed !";
+    } else if (secondTimeout < 60 ){
+        message = secondTimeout + " seconds have passed !";
+    } else if (minuteTimeout < 60){
+        message = minuteTimeout + " minutes have passed ! " + secondTimeout + " seconds !";
+    }
+    document.getElementById("timer").textContent = message;
+}
+
+setInterval(timer, 1000);
