8ad72b7af40558d565f9966b6e5f4cd8f3592f12
Mathias Barbier <mathbarbier07@gmail.com>
2023-09-30 16:24:20 +0200

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,15 +2,27 @@ let currStalinTile;
 let currGopnikTile;
 let score = 0;
 let gameOver = false;
+let backgroundMusic = document.getElementById('backgroundMusic');
 
 
 window.onload = function() {
     setGame();
+    playMusic();
+    pauseMusic();
 }
+
+function playMusic() {
+    backgroundMusic.play();
+}
+
+function pauseMusic() {
+    backgroundMusic.pause();
+}
+
+
 
 function setGame() {
     for (let i = 0 ; i < 9; i++){
-
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener('click', selectTile);
@@ -35,7 +47,7 @@ function setStalin() {
     }
 
     let stalin = document.createElement('img');
-    stalin.src = 'assets/img/stalin.png';
+    stalin.src = './assets/img/stalin.png';
     let num = getRandomTiles();
 
     if (currGopnikTile && currGopnikTile.id == num){
@@ -56,7 +68,7 @@ function setGopnik() {
     }
 
     let gopnik = document.createElement('img');
-    gopnik.src = 'assets/img/gopnik.png';
+    gopnik.src = './assets/img/gopnik.png';
     let num = getRandomTiles();
 
     if (currStalinTile && currStalinTile.id == num){
@@ -73,13 +85,18 @@ function selectTile() {
     }
 
     if (this == currStalinTile){
+        let shoot = new Audio('./assets/sound/gun.mp3');
+        shoot.play();
         score += 50;
         document.getElementById('score').innerText = score.toString();
     } else if (this == currGopnikTile){
+        let blyat = new Audio('./assets/sound/blyat.mp3');
+        blyat.play();
         let desc = document.createElement('h3');
         document.getElementById('score').innerText ="GAME-OVER : " + score.toString();
         desc.innerText = "CYKA BLYAT";
         gameOver = true;
-        currGopnikTile.appendChild(desc);   
+        currGopnikTile.appendChild(desc);
     }
 }
+
